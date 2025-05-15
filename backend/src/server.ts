3975066f7d4f1fa2d0d238e4a8f10e639ef08ba2
mohamedmufeed <mohamedmufeed44@gmail.com>
2025-05-15 10:20:08 +0530

    fix:hosting ip fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -43,9 +43,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "http://13.204.5.195:5173"],
         credentials: true,
     })
 );
